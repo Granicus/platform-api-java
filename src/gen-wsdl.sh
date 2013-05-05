b77a64aee2e3bac084d4aed1e534a1230b468e2e
Javier Muniz <javier@granicus.com>
2013-05-04 21:25:03 -0700

    added Performance Acclerator API

--- src/gen-wsdl.sh
+++ src/gen-wsdl.sh
@@ -7,3 +7,4 @@ done
 #echo "classpath = $CP"
 java -cp $CP org.apache.axis.wsdl.WSDL2Java granicus.wsdl
 java -cp $CP org.apache.axis.wsdl.WSDL2Java encoder.wsdl --package com.granicus.encoder
+java -cp $CP org.apache.axis.wsdl.WSDL2Java accelerator.wsdl --package com.granicus.accelerator
